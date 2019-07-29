1d004feca6ab55fac3a3dd8244d0cf6676f79c3f
ktnguyen2 <43625208+ktnguyen2@users.noreply.github.com>
2019-07-29 12:36:33 -0700

    Create virtualenv.sh

---
+++ virtualenv.sh
@@ -0,0 +1,12 @@
+# create virtual environment
+virtualenv mypython # mypython is name of local directory
+
+# I set up virtual environment in /home/nvidianav directory
+virtualenv ./
+
+# activate
+source mypython/bin/activate
+source ./bin/activate
+
+# deactivate
+deactivate
